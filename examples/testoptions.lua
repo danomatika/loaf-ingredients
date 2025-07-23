729ee32f87046e9c265d786e87765ea5bb172e3c
danomatika <danomatika@gmail.com>
2025-07-23 14:40:29 +0200

    options: added support for non-value flags, updated testoptions

--- examples/testoptions.lua
+++ examples/testoptions.lua
@@ -1,9 +1,26 @@
 -- run with args, ex:
--- loaf examples/testoptions.lua --foo bar -b -a z -nuc one two three
+-- loaf examples/testoptions.lua --foo bar --baz 1 -a knapp -u 432.1 -t one two three
 
 package.path = package.path .. ";../?.lua;../?/init.lua"
 
 local options = require "options"
+
+local helptext = [[
+Usage: loaf myscript [OPTIONS] [ARGUMENT...]
+
+my loaf script
+
+Options:
+  -h, --help    print this help
+  -f, --foo     string option
+  --baz         number option
+  -a            string option
+  -u            number option
+  -t            flag, no value
+
+Arguments:
+  ARGUMENT      additional non-option arguments
+]]
 
 -- args
 io.write(#arg.." args:")
@@ -13,11 +30,29 @@ end
 io.write("\n")
 
 -- options
-local opt, argi = options.parse(arg)
+local opt, argi = options.parse(arg, {"h", "help", "t"})
 local optc = 0
 print("options")
 for k,v in pairs(opt) do
 	print("  "..k..": "..tostring(v).." ("..type(v)..")")
+	if k == "h" or k == "help" then
+		io.write(helptext)
+		os.exit(0)
+	elseif k == "f" or k == "foo" then
+		--
+	elseif k == "baz" then
+		if tonumber(v) == nil then
+			print("option baz invalid")
+		end
+	elseif k == "a" then
+		--
+	elseif k == "u" then
+		if tonumber(v) == nil then
+			print("option u invalid")
+		end
+	elseif k == "t" then
+		--
+	end
 	optc = optc + 1
 end
 print("option count: "..optc)
--- options.lua
+++ options.lua
@@ -1,8 +1,8 @@
--- VERSION     = options 1.0.0
+-- VERSION     = options 1.2.0
 -- DESCRIPTION = super simple commandline options parser
 -- URL         = http://github.com/danomatika/loaf-ingredients
 -- LICENSE     =
---   Copyright (c) 2022 Dan Wilcox <danomatika@gmail.com> MIT License.
+--   Copyright (c) 2022-2025 Dan Wilcox <danomatika@gmail.com> MIT License.
 --   For information on usage and redistribution, and for a DISCLAIMER OF ALL
 --   WARRANTIES, see the file, "LICENSE.txt," in this distribution.
 
@@ -10,62 +10,82 @@ local options = {}
 
 -- parse options hash from arg string array
 -- * key: short "-" and long "--" options
+-- * value: true if key is in optional flags array, or
 -- * (assumed) value based on next arg:
 --   - next arg is option: true (assume flag)
---   - next arg *not* option: use as value (number or string)  
+--   - next arg *not* option: use as value (number or string)
 -- * short flags can be compounded: -abc = -a -b -c
 -- * stops parsing on "--" or first non-value, ex. "-f bar baz -c" stops on baz
+--
+-- flags: a list of options which take no arguments and will be set to true if
+--        found, otherwise not set ie. nil
+--
+-- ex: local opt, argi = options.parse(arg, {"a"})
+--     "--foo 123 -a -bar abc -- baz" -> {"foo"=123, "bar"="abc", "a"=true}, 7
+--
 -- returns options key/value hash & first non-option pos in arg array
--- ex: "--foo 123 -bar abc -a -- baz" -> {"foo"=123, "bar"="abc", "a"=true}, 7
 -- non-options pos is 0 if no remaining args
-function options.parse(arg)
-	local opt = {}
-	local i = 1 -- arg index
-	while i <= #arg do
-		if arg[i] == "--" then
-			-- stop parsing
-			i = i + 1
-			break
-		end
-		local key = nil
-		if string.sub(arg[i], 1, 2) == "--" then
-			-- long opt
-			key = string.sub(arg[i], 3, string.len(arg[i]))
-		elseif string.sub(arg[i], 1, 1) == "-" then
-			-- short opt
-			key = string.sub(arg[i], 2, string.len(arg[i]))
-			if string.len(key) > 1 then
-				-- compound short flags: -abc = -a -b -c
-				while string.len(key) > 0 do
-					local k = string.sub(key, 1, 1)
-					opt[k] = true
-					key = string.sub(key, 2, string.len(key))
-				end
-				key = nil
-			end
-		else
-			-- non-option, stop parsing
-			break
-		end
-		i = i + 1
-		if key then
-			if arg[i] == nil or options.isopt(arg[i]) then
-				-- assume flag
-				opt[key] = true
-			else
-				-- value
-				opt[key] = tonumber(arg[i]) or arg[i]
-				i = i + 1
-			end
-		end
-	end
-	if i > #arg then i = 0 end
-	return opt, i
+function options.parse(arg, flags)
+    local opt = {}
+    local i = 1 -- arg index
+    while i <= #arg do
+        if arg[i] == "--" then
+            -- stop parsing
+            i = i + 1
+            break
+        end
+        local key = nil
+        if string.sub(arg[i], 1, 2) == "--" then
+            -- long opt
+            key = string.sub(arg[i], 3, string.len(arg[i]))
+        elseif string.sub(arg[i], 1, 1) == "-" then
+            -- short opt
+            key = string.sub(arg[i], 2, string.len(arg[i]))
+            if string.len(key) > 1 then
+                -- compound short flags: -abc = -a -b -c
+                while string.len(key) > 0 do
+                    local k = string.sub(key, 1, 1)
+                    opt[k] = true
+                    key = string.sub(key, 2, string.len(key))
+                end
+                key = nil
+            end
+        else
+            -- non-option, stop parsing
+            break
+        end
+        i = i + 1
+        if key then
+            if flags and options.contains(flags, key) then
+                -- flag
+                opt[key] = true
+            elseif arg[i] == nil or options.isopt(arg[i]) then
+                -- assume flag
+                opt[key] = true
+            else
+                -- value
+                opt[key] = tonumber(arg[i]) or arg[i]
+                i = i + 1
+            end
+        end
+    end
+    if i > #arg then i = 0 end
+    return opt, i
 end
 
 -- returns true if given arg string is an option
 function options.isopt(arg)
-	return (string.sub(arg, 1, 2) == "--") or (string.sub(arg, 1, 1) == "-")
+    return (string.sub(arg, 1, 2) == "--") or (string.sub(arg, 1, 1) == "-")
+end
+
+-- returns true if table contains value
+function options.contains(table, value)
+    for i=1,#table do
+        if table[i] == value then
+            return true
+        end
+    end
+    return false
 end
 
 return options
